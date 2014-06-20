67e1e97dd40341f79053722afdd353b04e866417
Vladimir Kiselev <nettsundere@gmail.com>
2014-06-20 04:18:49 +0400

    Removed deriving show (no longer needed)

--- src/Display.hs
+++ src/Display.hs
@@ -3,8 +3,8 @@ module Display where
 
 import Foreign.C(CInt(..), CFloat(..))
 
-newtype DisplayNumber = DisplayNumber CInt deriving(Show)
-newtype Brightness = Brightness CFloat deriving(Show)
+newtype DisplayNumber = DisplayNumber CInt
+newtype Brightness = Brightness CFloat
 
 foreign import ccall getBrightness :: DisplayNumber -> IO Brightness
 foreign import ccall setBrightness :: DisplayNumber -> Brightness -> IO()
